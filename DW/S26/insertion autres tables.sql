0b2582933ff17df87a8f898e5bc730d0bc5eef7a
emilieturmine <82571837+emilieturmine@users.noreply.github.com>
2021-06-30 16:59:11 +0200

    Add files via upload

---
+++ DW/S26/insertion autres tables.sql
@@ -0,0 +1,17 @@
+INSERT INTO categories (cat_id, cat_name, cat_parent_id)
+VALUES (,'',);(,'',);(,'',);
+
+INSERT INTO customers (cus_id, cus-lastname, cus_firstname, cus_zipcode, cus_city, cus_countries_id, cus_mail, cus_phone, cus_password, cus_add_date, cus_update_date)
+VALUES (,'','',,'','',,'',,);(,'','',,'','',,'',,);(,'','',,'','',,'',,);
+
+INSERT INTO employees (emp_id, emp_superior_id, emp_pos_id, empl_lastname, emp_firstname, emp_adress, emp_zipcode, emp_city, emp_mail, emp_phone, emp_salary, emp_enter_date, emp_gender, emp_children)
+VALUES (,,,'','','',,'','', , ,,'',);(,,,'','','',,'','', , ,,'',),(,,,'','','',,'','', , ,,'',);
+
+INSERT INTO orders (ord_id, ord_order_date, ord_payment_date, ord_ship_date, ord_reception_date, ord_status, ord_cus_id)
+VALUES(,,,,,'',);(,,,,,'',);(,,,,,'',);
+
+INSERT INTO posts (pos_id, pos_libelle)
+VALUES (,'');(,'');(,'');
+
+INSERT INTO suppliers (sup_id, sup_name, sup_city, sup_countries_id, sup_address, sup_zipcode, sup_contact, sup_phone, sup_mail )
+VALUES (,'','',,'',,'',,'');
