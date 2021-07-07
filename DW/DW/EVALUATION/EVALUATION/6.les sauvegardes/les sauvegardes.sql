e3f5804859598a8026b7c1928b900f16f30d8e26
emilieturmine <82571837+emilieturmine@users.noreply.github.com>
2021-07-07 11:46:01 +0200

    Add files via upload

---
+++ DW/DW/EVALUATION/EVALUATION/6.les sauvegardes/les sauvegardes.sql
@@ -0,0 +1,4 @@
+mysqldump -u emilie_turmine -p gescom_afpa > /home/stagiaire/backup/tata.sql
+/* pour sauvegarge manuelle*/
+ mysql -u emilie_turmine -p gescom afpa < /home/stagiaire/backup/tata.sql
+ /*pour restaurer/*
