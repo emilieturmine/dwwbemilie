38f386cae970f5d4913e41953dcbf87a9b27b53b
emilieturmine <82571837+emilieturmine@users.noreply.github.com>
2021-07-07 11:46:42 +0200

    Delete DW/DW/EVALUATION directory

--- DW/DW/EVALUATION/EVALUATION/6.les sauvegardes/les sauvegardes.sql
+++
@@ -1,4 +0,0 @@
-mysqldump -u emilie_turmine -p gescom_afpa > /home/stagiaire/backup/tata.sql
-/* pour sauvegarge manuelle*/
- mysql -u emilie_turmine -p gescom afpa < /home/stagiaire/backup/tata.sql
- /*pour restaurer/*
