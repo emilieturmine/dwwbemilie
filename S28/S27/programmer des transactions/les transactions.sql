9ebb68fc5a3500e806b7a00122346b18597aafb2
emilieturmine <82571837+emilieturmine@users.noreply.github.com>
2021-07-13 10:47:41 +0200

    Delete S28/S27 directory

--- S28/S27/programmer des transactions/les transactions.sql
+++
@@ -1,8 +0,0 @@
-START TRANSACTION;
-	
-SELECT nomfou FROM fournis WHERE numfou = 120;    
-	
-UPDATE fournis SET nomfou = 'GROSBRIGAND' WHERE numfou = 120;
-	
-SELECT nomfou FROM fournis WHERE numfou = 120; 
-Commit;
