0edad7f2b249da05c39890c222d26bbb63a6ff4a
emilieturmine <82571837+emilieturmine@users.noreply.github.com>
2021-08-15 20:39:13 +0200

    Add files via upload

---
+++ phpavancé/form_ajout.php
@@ -0,0 +1,5 @@
+<?php 
+/* reste a faire :
+creer un formulaire pour ajouter les infos pour chaque colone de la table produit
+attention les valeurs entrées doivent correspondre aux types des collonnes dc gros travail de verification avant validation
+le clic sur le bouton envoyer declenche l'appel de la page specifié dans action (scriptajout.php)
