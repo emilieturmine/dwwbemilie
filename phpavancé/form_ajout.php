5925c426560bee5659454d0bcfe2e5a06340076d
emilieturmine <82571837+emilieturmine@users.noreply.github.com>
2021-08-15 20:39:39 +0200

    Delete phpavancé directory

--- phpavancé/form_ajout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-/* reste a faire :
-creer un formulaire pour ajouter les infos pour chaque colone de la table produit
-attention les valeurs entrées doivent correspondre aux types des collonnes dc gros travail de verification avant validation
-le clic sur le bouton envoyer declenche l'appel de la page specifié dans action (scriptajout.php)
--- phpavancé/formmodif.php
+++
@@ -1,8 +0,0 @@
-<!--
-    reste a faire :
-    Creer un formulaire de modification
-
-    Il doit permettre de modifier toutes les informations présentes dans la table produits.
-    Vous devez donc créer un formulaire et l'appeler par exemple formulaire_modif.html. 
-    Un clic sur le bouton Envoyer déclenche l'appel de la page spécifiée dans l'attribut action, par exemple _scriptmodif.php.
--->
