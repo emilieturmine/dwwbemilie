adff29837092dbb1d7fcff1bc4a265e5b7325a34
emilieturmine <82571837+emilieturmine@users.noreply.github.com>
2021-08-16 16:47:11 +0200

    Delete script_modif.php

--- script_modif.php
+++
@@ -1,8 +0,0 @@
-reste a faire 
-
-Ce script doit effectuer les actions suivantes :
-
-Récupération des informations transmises par le formulaire.
-Enregistrement des données dans la base (à l'aide d'une requête UPDATE).
-La date d'ajout ne doit pas être modifiée, par contre cette fois il faut renseigner la date de modification.
-Redirection vers la page de liste.
